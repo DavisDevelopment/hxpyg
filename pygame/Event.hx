31e73ace7556da64d5613341814d07a28a926beb
Ryan Davis <epikit@gmail.com>
2015-05-17 01:19:38 -0500

    Reimplemented pygame.Event enum, with more robust construct arguments

--- pygame/Event.hx
+++ pygame/Event.hx
@@ -1,33 +1,36 @@
 package pygame;
 
 import tannus.geom.Point;
+import tannus.geom.Area;
 import tannus.io.Byte;
 import tannus.ds.Object;
+import tannus.inputs.MouseButton;
+import tannus.events.EventMod;
 
 /**
   * Enum of all possible pygame Events
   */
 enum Event {
 	/* Mouse Motion */
-	MouseMove(npos : Point);
+	MouseMove(pos:Point, from:Point, buttons:Array<Int>);
 
 	/* Mouse Down */
-	MouseDown(pos:Point, button:Int);
+	MouseDown(pos:Point, button:MouseButton);
 
 	/* Mouse Up */
-	MouseUp(pos:Point, button:Int);
+	MouseUp(pos:Point, button:MouseButton);
 
 	/* Key Down */
-	KeyDown(key:Byte);
+	KeyDown(key:Byte, mods:Array<EventMod>);
 
 	/* Key Up */
-	KeyUp(key:Byte);
+	KeyUp(key:Byte, mods:Array<EventMod>);
 
 	/* Unknown Event */
 	Other(type:Int, data:Object);
 
 	/* Resize Window */
-	Resize;
+	Resize(nsize : Area);
 
 	/* Close Window */
 	Quit;
